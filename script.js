60ddb4499c1acd737e88039dbd3f88a343b0e827
Harsh Rajak <88426085+Harshrajak@users.noreply.github.com>
2024-01-12 21:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 let hourHand = document.querySelector('#hr-hand');
 let minuteHand = document.querySelector('#min-hand');
 let secondHand = document.querySelector('#sec-hand');
+
+let dayBox = document.querySelector('.day');
+let monthBox = document.querySelector('.month');
+let yearBox = document.querySelector('.year');
 
 let hrRotation;
 let minRotation;
@@ -14,9 +21,18 @@ setInterval(() => {
   let min = d.getMinutes();
   let sec = d.getSeconds();
 
+  let date = d.getDate().toString().padStart(2, '0');
+  let day = d.getDay();
+  let month = d.getMonth();
+  let year = d.getFullYear();
+
   hrRotation = 30 * hr + min / 2;
   minRotation = 6 * min;
   secRotation = 6 * sec;
+
+  dayBox.textContent = `${week[day]},`;
+  monthBox.textContent = `${months[month]} ${date},`;
+  yearBox.textContent = `${year}`;
 
   hourHand.style.transform = `rotate(${hrRotation}deg)`;
   minuteHand.style.transform = `rotate(${minRotation}deg)`;
